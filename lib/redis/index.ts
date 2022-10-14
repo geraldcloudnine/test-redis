bf50a31ac5ccc675a51ee3452367bed137af7e32
Gerald Andrei Tamasi <gerald.tamasi@cloudnine.se>
2022-10-14 09:13:56 +0200

    cleanup

--- lib/redis/index.ts
+++ lib/redis/index.ts
@@ -1,10 +1,7 @@
 import Redis from "ioredis";
 
 const API = process.env.NEXT_PUBLIC_REDIS_URL || "";
-const TOKEN = process.env.NEXT_PUBLIC_REDIS_TOKEN || "";
 
-const redis = new Redis(
-  "rediss://:572b262a80824e568ebb659de712fb9e@eu2-loved-rabbit-31021.upstash.io:31021"
-);
+const redis = new Redis(API);
 
 export default redis;
